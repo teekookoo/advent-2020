96344f63f9f62ba155801094014587f77c0e4410
Tarmo Kivioja <tarmo.kivioja@gmail.com>
2020-12-01 13:30:43 +0200

    Minor changes

--- src/Day01.hs
+++ src/Day01.hs
@@ -4,7 +4,7 @@ module Day01
   ) where
 
 input :: IO [Int]
-input = stringToInts <$> readFile "inputs/input01"
+input = stringToInts <$> readFile "src/inputs/input01"
 
 solve1 :: IO ()
 solve1 = do
@@ -16,11 +16,11 @@ solve1 = do
 stringToInts :: String -> [Int]
 stringToInts = map read . lines
 
-sumsAndProducts :: [Int] -> [(Int, Int)]
+sumsAndProducts :: Num a => [a] -> [(a, a)]
 sumsAndProducts [] = []
 sumsAndProducts (x:xs) = map (\y -> (x+y, x*y)) xs ++ (sumsAndProducts xs)
 
-findProduct :: [(Int, Int)] -> Maybe Int
+findProduct :: (Eq a, Num a) => [(a, a)] -> Maybe a
 findProduct = lookup 2020
   
 
@@ -31,7 +31,7 @@ solve2 = do
     Just p -> putStrLn (show p)
     Nothing -> putStrLn "No 3 lines sum to 2020"
 
-sumsAndProducts2 :: [Int] -> [(Int, Int)]
+sumsAndProducts2 :: Num a => [a] -> [(a, a)]
 sumsAndProducts2 [] = []
 sumsAndProducts2 (x:xs) = zip sums2 products2 ++ sumsAndProducts2 xs
   where (sums, products) = unzip (sumsAndProducts xs)
