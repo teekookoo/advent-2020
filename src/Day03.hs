df7260e7f81e57a5296f9a97999b93d68085d11a
Tarmo Kivioja <tarmo.kivioja@gmail.com>
2020-12-03 18:42:08 +0200

    Day 3 fully done

--- src/Day03.hs
+++ src/Day03.hs
@@ -7,15 +7,18 @@ import Data.Set (Set)
 import qualified Data.Set as S
 
 data TreeMap = TreeMap Int Int (Set (Int, Int))
-type Slope = Int
+type Slope = (Int, Int)
 
 solve1 :: IO ()
 solve1 = do
   tm <- input
-  putStrLn . show $ treesOnPath tm 3
+  print $ treesOnPath tm (3, 1)
 
 solve2 :: IO ()
-solve2 = putStrLn "Not implemented"
+solve2 = do
+  tm <- input
+  let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+  print . product $ map (treesOnPath tm) slopes
 
 input :: IO TreeMap
 input = do
@@ -24,10 +27,11 @@ input = do
   let width = length (head inputList)
   let indexList = [(y, x) | y <- [0..height-1], x <- [0..width-1]]
   let indexedInput = indexList `zip` (concat inputList)
-  let treeCoordinates = S.fromList . map fst . filter ((== '#') . snd) $ indexedInput
-  return (TreeMap width height treeCoordinates)
+  let treeCoordinates = S.fromList . map fst . filter ((== '#') . snd)
+  return (TreeMap width height (treeCoordinates indexedInput))
 
 treesOnPath :: TreeMap -> Slope -> Int
-treesOnPath (TreeMap width height trees) dx = S.size $ S.intersection trees path
+treesOnPath (TreeMap width height trees) (dx, dy) =
+  S.size $ S.intersection trees (S.fromList path)
   where
-    path = S.fromList [(y, y * dx `mod` width) | y <- [0..height-1]]
+    path = [(i * dy, i * dx `mod` width)| i <- [0 .. height - 1 `div` dy]]
